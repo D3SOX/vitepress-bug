7ddd8daf5c3679c411989bcd83d15775f7604833
D3SOX <nico@d3sox.me>
2022-08-17 13:06:55 +0200

    initial commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+    lang: 'en-US',
+    title: 'VitePress Starter',
+    description: 'Test',
+
+    themeConfig: {
+        footer: {
+            message: 'Footer message',
+            //copyright: 'No copyright set',
+        },
+    },
+});
